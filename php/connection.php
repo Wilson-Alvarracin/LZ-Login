8b54a0519d0645f7b334a863359a163706497da7
jovico7 <100007903.joan23@fje.edu>
2023-11-10 18:23:30 +0100

    toda la media

--- php/connection.php
+++ php/connection.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = mysqli_connect("172.24.1.28", "zorrito","QWEqwe123","bd_zorritos");
+$conn = mysqli_connect("172.24.0.136", "zorrito","QWEqwe123","bd_zorritos");
--- php/media.php
+++ php/media.php
@@ -18,16 +18,51 @@ include 'connection.php';
     <title>Media</title>
 </head>
 <body>
-<?php
-  if ($_SESSION['user'] == "admin@fje.edu") {
-    ?>
-    <div class="login-card center-mostrar">
+<div class="login-card center-mostrar">
         <div class="row custom-form-container container">
             <div class="responsive-img-center">
             <form action="./mostrar.php" method='post' style="text-align: right;">
             <button type="submit" name="Media" value="Media">Volver</button>
             </form>
             </div>
+<?php
+
+  if ($_SESSION['user'] == "admin@fje.edu") {
+    // Si el usuario es "admin@fje.edu", entramos
+    
+    // Con esta consulta obtenemos la lista de asignaturas que hay
+    $sql = "SELECT DISTINCT materia FROM tbl_notas;";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Si hay asignaturas en la base de datos, las almacenamos en un array
+        $column_names = array();
+        while ($row = $result->fetch_assoc()) {
+            $column_names[] = $row["materia"];
+        }
+
+        // Para cada asignatura calculamos el promedio de las notas
+        foreach ($column_names as $column_name) {
+            // Preparamos la consulta SQL para calcular el promedio de notas para X asignatura
+            $query ="SELECT AVG(nota) AS media FROM tbl_notas WHERE materia = ?;";
+            $stmt = mysqli_prepare($conn, $query);
+            mysqli_stmt_bind_param($stmt, "s", $column_name);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            if ($result) {
+                // Si la consulta se ejecuta correctamente obtendremos aquí el resultado y mostraremos el promedio
+                $row = $result->fetch_assoc();
+                $media_asignatura = $row["media"];
+                echo "<b>Media de " . $column_name . ": " . $media_asignatura . "</b><br>";
+            }
+            $stmt->close();
+        }
+    } else {
+        // Si no se encontraron asignaturas en la base de datos, mostramos este mensaje
+        echo "No se encontraron asignaturas.";
+    }
+    ?>
         </div>
     </div>
 <?php
