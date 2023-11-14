4149acf03e44953ebf630aa84c9f81346643e446
Sergi-Leon <75490491+Sergi-Leon@users.noreply.github.com>
2023-11-14 20:53:43 +0100

    Correo electronico

--- php/correo.php
+++ php/correo.php
@@ -15,19 +15,12 @@ include 'connection.php';
     <script src="./javascript/javascript.js"></script>
     <link rel="stylesheet" href="../css/test.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title>Alumno</title>
+    <title>Correo electronico</title>
 </head>
 <body>
     <?php
     //comentario de prueba
     if ($_SESSION['user'] == "admin@fje.edu") {
-        if (isset($_POST["materia"]) && $_POST["materia"] != "Todo") {
-            // Filtrar por materia si se ha seleccionado una
-            $filtroMateria = $_POST["materia"];
-        } else {
-            $filtroMateria = null;
-        }
-        
         if (isset($_POST["buscar_nombre"])) {
             // Filtrar por nombre si se ha ingresado un nombre
             $filtroNombre = '%' . $_POST["buscar_nombre"] . '%';
@@ -35,28 +28,18 @@ include 'connection.php';
             $filtroNombre = null;
         }
         
-        if ($filtroMateria || $filtroNombre) {
+        if ($filtroNombre) {
             // Consulta SQL con filtro de materia y/o nombre
-            $sql = "SELECT a.id_alumno, a.nombre, a.apellidos, n.materia, n.nota
+            $sql = "SELECT a.id_alumno, a.nombre, a.apellidos, a.correo_electronico
                     FROM tbl_alumnos a
-                    INNER JOIN tbl_notas n ON a.id_alumno = n.id_alumno
                     WHERE 1=1";
-            
-            if ($filtroMateria) {
-                $sql .= " AND n.materia = ?";
-            }
-            
             if ($filtroNombre) {
                 $sql .= " AND a.nombre LIKE ?";
             }
         
             $stmt = mysqli_prepare($conn, $sql);
         
-            if ($filtroMateria && $filtroNombre) {
-                mysqli_stmt_bind_param($stmt, "ss", $filtroMateria, $filtroNombre);
-            } elseif ($filtroMateria) {
-                mysqli_stmt_bind_param($stmt, "s", $filtroMateria);
-            } else {
+            if ($filtroNombre) {
                 mysqli_stmt_bind_param($stmt, "s", $filtroNombre);
             }
         
@@ -64,9 +47,8 @@ include 'connection.php';
             $result = mysqli_stmt_get_result($stmt);
         } else {
             // Consulta SQL sin filtro (mostrar todos los resultados)
-            $sql = "SELECT a.id_alumno, a.nombre, a.apellidos, n.materia, n.nota
-                    FROM tbl_alumnos a
-                    INNER JOIN tbl_notas n ON a.id_alumno = n.id_alumno";
+            $sql = "SELECT a.id_alumno, a.nombre, a.apellidos, a.correo_electronico
+                    FROM tbl_alumnos a";
             $stmt = mysqli_prepare($conn, $sql);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
@@ -79,44 +61,35 @@ include 'connection.php';
 <form method="post">
     <label for="buscar_nombre">Buscar por Nombre:</label>
     <input type="text" id="buscar_nombre" name="buscar_nombre">
-    <label for="materia">Buscar por Materia:</label>
-    <select name="materia">
-        <option value="Todo">Todo</option>
-        <option value="Matemáticas">Matemáticas</option>
-        <option value="Historia">Historia</option>
-    </select>
-    <button type="submit" name="filtro_materia" value="Filtrar">Filtrar</button>
+    <button type="submit" name="filtroNombre" value="Filtrar">Filtrar</button>
 </form>
 <br>
-<button type="button" class="btn btn-info" onclick="window.location.href='./alumno/crearAlu.php'">Crear Alumno</button>   
-<button type="button" class="btn btn-success" onclick="window.location.href='./correo.php'">Correo electronico</button>
+<button type="button" class="btn btn-success" onclick="window.location.href='./alumno/formCorreo.php'">Enviar Correo</button>
 <br>
 <table class="table">
     <thead class="table-dark">
         <tr>
+            <th></th>
             <th class="azul">Nombre</th>
             <th>Apellidos</th>
-            <th>Materia</th>
-            <th>Nota</th>
-            <th>Modificar</th>
-            <th>Eliminar</th>
+            <th>Correo</th>
         </tr>
     </thead>
     <tbody><?php
     // Recorre los resultados y muestra los datos en la tabla
     foreach ($result as $row) {
         echo "<tr>";
+        echo "<td><input type='checkbox' name='seleccionados[]' value='" . $row['id_alumno'] . "'></td>";
         echo "<th style='font-size: 14px;'>" . $row['nombre'] ."</th>";
         echo "<td style='font-size: 14px;'>" . $row['apellidos'] ."</td>";
-        echo "<td style='font-size: 14px;'>" . $row['materia'] . "</td>";
-        echo "<td style='font-size: 14px;'>" . $row['nota'] . "</td>";
-        echo '<td> <button type="button" class="btn btn-warning" onclick="window.location.href=\'./alumno/modificar.php?id='.$row['id_alumno'].'&materia='.$row['materia'].'\'">Modificar</button> </td>';        
-        echo '<td> <button type="button" class="btn btn-danger" onclick="window.location.href=\'./alumno/eliminar.php?id=' . $row['id_alumno'] . '&materia=' . $row['materia'] . '\'">Eliminar</button> </td>';
+        echo "<td style='font-size: 14px;'>" . $row['correo_electronico'] . "</td>";
         echo "</tr>";
+
     }
     ?>
         </tbody>
     </table>
+    <button type="button" class="btn btn-danger" onclick="window.location.href='./mostrar.php'">Volver</button>
         </div>
     </div>
 </div>
